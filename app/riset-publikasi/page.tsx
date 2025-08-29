7f5f1201e3c13f8bd73c630dff23d289a5cbee98
Kholen <kholenlim28@gmail.com>
2025-08-29 10:29:31 +0700

    menambahkan page riset-publikasi

---
+++ app/riset-publikasi/page.tsx
@@ -0,0 +1,8 @@
+import { title } from "@/components/primitives";
+import CardDrop from "@/components/CardDrop";
+
+export default function BlogPage() {
+  return <div className="container-fluid">
+    
+  </div>;
+}
