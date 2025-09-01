ed5fcfa2cfa7136eb64b297929997ca22ccd3f9b
YuriFerdinand <muhammadyuri965@gmail.com>
2025-09-01 11:35:58 +0700

    memperbaiki error dan mengubah sistem input data pada riset & publikasi

--- app/riset-publikasi/page.tsx
+++ app/riset-publikasi/page.tsx
@@ -1,4 +1,19 @@
 export default function BlogPage() {
+  const isiTable = [
+    {
+      judul:  'Penelitian tentang Lorem ipsum dolor, sit amet consectetur adipisicing',
+      namaKP: 'Muhammad Yuri Ferdinand Adinata',
+      tahun: '2025',
+      url: 'https://forum.upbatam.ac.id/index.php/prosiding/article/view/5202'
+    },
+    {
+      judul:  'Penelitian tentang Lorem ipsum dolor, sit amet consectetur adipisicing',
+      namaKP: 'Ahmed',
+      tahun: '2025',
+      url: 'https://forum.upbatam.ac.id/index.php/prosiding/article/view/5202'
+    }
+  ]
+
   return (
     <>
       <div className="w-full max-w-5xl mx-auto overflow-hidden rounded-lg shadow-md">
@@ -24,83 +39,38 @@ export default function BlogPage() {
         </thead>
       
       <tbody>
-        <tr className="bg-white border-b hover:bg-gray-50">
-          <td className="py-4 px-6 font-medium">
-            1
-          </td>
-          <th scope="row" className="py-4 px-6 font-medium text-gray-900">
-            Penelitian tentang Lorem ipsum dolor, sit amet consectetur adipisicing.
-          </th>
-          <td className="py-4 px-6">
-            Muhammad Yuri Ferdinand Adinata
-          </td>
-          <td className="py-4 px-6">
-            2025
-          </td>
-          <td className="py-4 px-6">
-            <a href="https://forum.upbatam.ac.id/index.php/prosiding/article/view/5202" target="_blank" rel="noopener noreferrer" className="text-blue-600 hover:underline">
-              Lihat Publikasi
-            </a>
-          </td>
-        </tr>
-        <tr className="bg-white border-b hover:bg-gray-50">
-          <td className="py-4 px-6 font-medium">
-            1
-          </td>
-          <th scope="row" className="py-4 px-6 font-medium text-gray-900">
-            Penelitian tentang Lorem ipsum dolor, sit amet consectetur adipisicing.
-          </th>
-          <td className="py-4 px-6">
-            Muhammad Yuri Ferdinand Adinata
-          </td>
-          <td className="py-4 px-6">
-            2025
-          </td>
-          <td className="py-4 px-6">
-            <a href="https://forum.upbatam.ac.id/index.php/prosiding/article/view/5202" target="_blank" rel="noopener noreferrer" className="text-blue-600 hover:underline">
-              Lihat Publikasi
-            </a>
-          </td>
-        </tr>
-        <tr className="bg-white border-b hover:bg-gray-50">
-          <td className="py-4 px-6 font-medium">
-            1
-          </td>
-          <th scope="row" className="py-4 px-6 font-medium text-gray-900">
-            Penelitian tentang Lorem ipsum dolor, sit amet consectetur adipisicing.
-          </th>
-          <td className="py-4 px-6">
-            Muhammad Yuri Ferdinand Adinata
-          </td>
-          <td className="py-4 px-6">
-            2025
-          </td>
-          <td className="py-4 px-6">
-            <a href="https://forum.upbatam.ac.id/index.php/prosiding/article/view/5202" target="_blank" rel="noopener noreferrer" className="text-blue-600 hover:underline">
-              Lihat Publikasi
-            </a>
-          </td>
-        </tr>
-        <tr className="bg-white border-b hover:bg-gray-50">
-          <td className="py-4 px-6 font-medium">
-            1
-          </td>
-          <th scope="row" className="py-4 px-6 font-medium text-gray-900">
-            Penelitian tentang Lorem ipsum dolor, sit amet consectetur adipisicing.
-          </th>
-          <td className="py-4 px-6">
-            Muhammad Yuri Ferdinand Adinata
-          </td>
-          <td className="py-4 px-6">
-            2025
-          </td>
-          <td className="py-4 px-6">
-            <a href="https://forum.upbatam.ac.id/index.php/prosiding/article/view/5202" target="_blank" rel="noopener noreferrer" className="text-blue-600 hover:underline">
-              Lihat Publikasi
-            </a>
-          </td>
-        </tr>
-      
+        {isiTable.map((item, index) => (
+          <tr key={index} className="bg-white border-b hover:bg-gray-50">
+                
+                {/* Kolom untuk Nomor Urut */}
+                <td className="py-4 px-6 font-medium">
+                  {index + 1}
+                </td>
+                
+                {/* Kolom untuk Judul, menggunakan <td> bukan <th> */}
+                <td scope="row" className="py-4 px-6 font-medium text-gray-900">
+                  {item.judul}
+                </td>
+                
+                {/* Kolom untuk Nama Ketua Panitia (diperbaiki dari isi.judul menjadi isi.namaKP) */}
+                <td className="py-4 px-6">
+                  {item.namaKP}
+                </td>
+                
+                {/* Kolom untuk Tahun */}
+                <td className="py-4 px-6">
+                  {item.tahun}
+                </td>
+                
+                {/* Kolom untuk URL */}
+                <td className="py-4 px-6">
+                  <a href={item.url} target="_blank" rel="noopener noreferrer" className="text-blue-600 hover:underline">
+                    Lihat Publikasi
+                  </a>
+                </td>
+
+              </tr>
+            ))} 
       </tbody>
     </table>
   </div>
