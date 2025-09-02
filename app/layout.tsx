a59eddf5c1b4752561e0ddc3e34fa296e7f016d5
Kholen <kholenlim28@gmail.com>
2025-09-02 09:47:00 +0700

    menambahkan banner

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 import { Metadata, Viewport } from "next";
 import { Link } from "@heroui/link";
+import { Banner } from "@/components/Banner";
 import clsx from "clsx";
 
 import { Providers } from "./providers";
@@ -9,6 +10,7 @@ import { siteConfig } from "@/config/site";
 import { myFont } from "@/config/fonts";
 import { Navbar } from "@/components/navbar";
 import "../styles/globals.css";
+
 
 export const metadata: Metadata = {
   title: {
@@ -26,30 +28,42 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html suppressHydrationWarning lang="en">
       <head />
       <body className={clsx("min-h-screen text-foreground bg-background antialiased", myFont.className)}>
-          <div className="relative flex flex-col ">
-            <div className="bg-white sticky top-0 z-11">
-              <Navbar />
-            </div>
-            <div className="h-30">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#0A0950" fillOpacity="1" d="M0,64L48,74.7C96,85,192,107,288,96C384,85,480,43,576,42.7C672,43,768,85,864,101.3C960,117,1056,107,1152,101.3C1248,96,1344,96,1392,96L1440,96L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path></svg>
-            </div>
-
-            <main className="container mx-auto pt-10 px-6 flex-grow">{children}</main>
-
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#0A0950" fillOpacity="1" d="M0,192L60,181.3C120,171,240,149,360,160C480,171,600,213,720,213.3C840,213,960,171,1080,170.7C1200,171,1320,213,1380,234.7L1440,256L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path></svg>
-
-            <footer className="w-full flex items-center justify-center py-3">
-              <Link
-                isExternal
-                className="flex items-center gap-1 text-current"
-                href="https://heroui.com?utm_source=next-app-template"
-                title="heroui.com homepage"
-              >
-                <span className="text-default-600">Powered by</span>
-                <p className="text-primary">footer</p>
-              </Link>
-            </footer>
+        <div className="relative flex flex-col ">
+          <div className="bg-white sticky top-0 z-11">
+            <Navbar />
           </div>
+          
+          <Banner />
+
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+            <path
+              fill="#0A0950"
+              fillOpacity="1"
+              d="M0,64L48,74.7C96,85,192,107,288,96C384,85,480,43,576,42.7C672,43,768,85,864,101.3C960,117,1056,107,1152,101.3C1248,96,1344,96,1392,96L1440,96L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"
+            ></path>
+          </svg>
+          <main className="container mx-auto pt-10 px-6 flex-grow">{children}</main>
+
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+            <path
+              fill="#0A0950"
+              fillOpacity="1"
+              d="M0,192L60,181.3C120,171,240,149,360,160C480,171,600,213,720,213.3C840,213,960,171,1080,170.7C1200,171,1320,213,1380,234.7L1440,256L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"
+            ></path>
+          </svg>
+
+          <footer className="w-full flex items-center justify-center py-3">
+            <Link
+              isExternal
+              className="flex items-center gap-1 text-current"
+              href="https://heroui.com?utm_source=next-app-template"
+              title="heroui.com homepage"
+            >
+              <span className="text-default-600">Powered by</span>
+              <p className="text-primary">footer</p>
+            </Link>
+          </footer>
+        </div>
       </body>
     </html>
   );
