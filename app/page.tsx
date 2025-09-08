76f7ab877f817b06d4a5211dea272a6b79f62989
Kholen <kholenlim28@gmail.com>
2025-09-08 11:26:31 +0700

    perbaikan eror merge

--- app/page.tsx
+++ app/page.tsx
@@ -24,7 +24,6 @@ export default function Home() {
   const defaultContent2 =
     "ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
   return (
-    <>
       <div className="container">
         <Tabs
           className="w-full max-w-8xl mx-auto"
@@ -238,81 +237,6 @@ export default function Home() {
           </Tab>
         </Tabs>
 
-
-        {/* batas  background*/}
-        <svg className="w-screen relative left-1/2 -translate-x-1/2 -mb-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-          <path
-            fill="#0A0950"
-            fillOpacity="1"
-            d="M0,192L60,181.3C120,171,240,149,360,160C480,171,600,213,720,213.3C840,213,960,171,1080,170.7C1200,171,1320,213,1380,234.7L1440,256L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"
-          ></path>
-        </svg>
-
-              <div className="flex w-full flex-col pt-4">
-                <Tabs
-                  aria-label="Options"
-                  className="w-full max-w-8xl mx-auto line-bawah"
-                  classNames={{
-                    // Menghapus latar belakang abu-abu dan menambahkan garis batas bawah
-
-                    tabList: "relative w-full p-0 bg-white-500/10 rounded-none",
-
-                    // Ini adalah kunci utamanya: menata 'cursor' sebagai garis bawah
-                    cursor: "w-full h-1 bg-zinc-1000 ",
-                    // Anda bisa ganti bg-primary dengan warna lain, mis: bg-blue-500
-
-                    // Menyesuaikan padding pada setiap tab jika perlu
-                    tab: " h-12",
-
-                    // Mengubah warna teks dari tab yang aktif
-                    tabContent: "group-data-[selected=true]:text-white",
-                  }}
-                >
-                  <Tab key="photos" title="Deskripsi Program">
-                    <Card>
-                      <CardBody>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                        enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit
-                        amet consectetur adipisicing elit. Fugit vero facilis voluptates ut alias, hic repellendus assumenda quis quasi, sequi, eum
-                        nostrum veritatis! Aspernatur excepturi tempore esse labore quaerat necessitatibus.
-                      </CardBody>
-                    </Card>
-                  </Tab>
-                  <Tab key="videos" title="Keunggulan Prodi">
-                    <Card>
-                      <CardBody>
-                        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum
-                        dolor sit amet consectetur adipisicing elit. Quos, odio nostrum officiis excepturi doloribus iure unde atque molestias
-                        ratione, officia delectus non esse tempore expedita repellat. Sunt consequuntur earum dolore.
-                      </CardBody>
-                    </Card>
-                  </Tab>
-                </Tabs>
-              </div>
-            </CardBody>
-        </Tab>
-
-        {/* Tab 4: Dosen */}
-        <Tab key="kerjaSama" title="Kerja Sama Prodi">
-          <Card className="mainColor mt-2">
-            <CardBody>
-              <div className="flex flex-wrap gap-9.5 justify-center mt-5 mb-5 ">
-                <Image alt="HeroUI hero Image" src="https://heroui.com/images/hero-card-complete.jpeg" width={200} height={250} />
-                <Image alt="HeroUI hero Image" src="https://heroui.com/images/hero-card-complete.jpeg" width={200} height={250} />
-                <Image alt="HeroUI hero Image" src="https://heroui.com/images/hero-card-complete.jpeg" width={200} height={250} />
-                <Image alt="HeroUI hero Image" src="https://heroui.com/images/hero-card-complete.jpeg" width={200} height={250} />
-                <Image alt="HeroUI hero Image" src="https://heroui.com/images/hero-card-complete.jpeg" width={200} height={250} />
-                <Image alt="HeroUI hero Image" src="https://heroui.com/images/hero-card-complete.jpeg" width={200} height={250} />
-                <Image alt="HeroUI hero Image" src="https://heroui.com/images/hero-card-complete.jpeg" width={200} height={250} />
-                <Image alt="HeroUI hero Image" src="https://heroui.com/images/hero-card-complete.jpeg" width={200} height={250} />
-                <Image alt="HeroUI hero Image" src="https://heroui.com/images/hero-card-complete.jpeg" width={200} height={250} />
-                <Image alt="HeroUI hero Image" src="https://heroui.com/images/hero-card-complete.jpeg" width={200} height={250} />
-              </div>
-            </CardBody>
-          </Card>
-        </Tab>
-      </Tabs>
-
       <svg className="w-screen relative left-1/2 -translate-x-1/2 -mb-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
       <path fill="#0A0950" fillOpacity="1" d="M0,192L60,181.3C120,171,240,149,360,160C480,171,600,213,720,213.3C840,213,960,171,1080,170.7C1200,171,1320,213,1380,234.7L1440,256L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path></svg>
 
@@ -332,42 +256,35 @@ export default function Home() {
               </div>
 
               <div className="">
-              <div className="text-white font-bold text-2xl text-center rounded-lg bg-blue-800 w-30 p-2 my-2 hover justify-self-center">
-                    Visi
-                  </div>
-              <div className="bg-white rounded-lg w-auto">
-                <ul className="p-10 justify-content list-disc">
-                  <li>Menyelenggarakan pendidikan dan pengajaran yang berkualitas dengan terus mengikuti perkembangan ilmu dan teknologi khususnya di bidang Big Data dan Data Science.</li>
-                  <li>Menyiapkan mahasiswa untuk memasuki dunia kerja yang mampu bersaing baik lokal maupun nasional, mandiri dalam bidang Big Data dan Data Science, memiliki etos kerja tinggi, beretika, dan dapat bekerjasama dalam tim kerja.</li>
-                  <li>Menyelenggarakan penelitian yang dapat memberikan kontribusi terhadap masyarakat dan pengembangan dalam bidang Big Data dan Data Science serta Blockchain.</li>
-                  <li>Menyelenggarakan pengabdian kepada masyarakat sebagai kepedulian insan akademik dalam membantu meningkatkan kualitas masyarakat dalam bidang Big Data dan Data Science.</li>
-                  <li>Mengembangkan proses pembelajaran yang berkualitas berlandaskan Outcome Based Education (OBE).</li>
-                  <li>Menjalin kerjasama dalam pengembangan sumber daya manusia untuk menjembatani implementasi Big Data dan Data Science dalam konsep Smart City di Provinsi Kepulauan Riau.</li>
-                  <li>Mengembangkan kualitas SDM untuk memberikan layanan yang prima dalam bidang Big Data dan Data Science dan untuk menjadi seorang ilmuwan data.</li>
-                </ul>
-              </div>
+                <div className="text-white font-bold text-2xl text-center rounded-lg bg-blue-800 w-30 p-2 my-2 hover justify-self-center">
+                      Visi
+                    </div>
+                <div className="bg-white rounded-lg w-auto">
+                  <ul className="p-10 justify-content list-disc">
+                    <li>Menyelenggarakan pendidikan dan pengajaran yang berkualitas dengan terus mengikuti perkembangan ilmu dan teknologi khususnya di bidang Big Data dan Data Science.</li>
+                    <li>Menyiapkan mahasiswa untuk memasuki dunia kerja yang mampu bersaing baik lokal maupun nasional, mandiri dalam bidang Big Data dan Data Science, memiliki etos kerja tinggi, beretika, dan dapat bekerjasama dalam tim kerja.</li>
+                    <li>Menyelenggarakan penelitian yang dapat memberikan kontribusi terhadap masyarakat dan pengembangan dalam bidang Big Data dan Data Science serta Blockchain.</li>
+                    <li>Menyelenggarakan pengabdian kepada masyarakat sebagai kepedulian insan akademik dalam membantu meningkatkan kualitas masyarakat dalam bidang Big Data dan Data Science.</li>
+                    <li>Mengembangkan proses pembelajaran yang berkualitas berlandaskan Outcome Based Education (OBE).</li>
+                    <li>Menjalin kerjasama dalam pengembangan sumber daya manusia untuk menjembatani implementasi Big Data dan Data Science dalam konsep Smart City di Provinsi Kepulauan Riau.</li>
+                    <li>Mengembangkan kualitas SDM untuk memberikan layanan yang prima dalam bidang Big Data dan Data Science dan untuk menjadi seorang ilmuwan data.</li>
+                  </ul>
+                </div>
 
               </div>
 
             </div>
 
-
-            <div className="bg-gray-300 rounded-2xl w-80 h-75 pr-5 ml-25">
-
-  
             <div className="bg-gray-300 rounded-2xl w-80 h-75 pr-5 ml-25 hover">
 
               <div className="bg-gray-400 rounded-2xl w-80 h-75 p-5 -mt-5 -ml-5">
                 <Image alt="Img" src="/mentahan.png" className="-mt-15" />
               </div>
+
             </div>
           </div>
         </div>
       </div>
-    </>
-
-
     </div>
-
   );
 }
