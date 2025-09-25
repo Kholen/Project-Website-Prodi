b427e6fe1bcd22e3ac1b574b84f6eda8788feda6
YuriFerdinand <muhammadyuri965@gmail.com>
2025-09-25 10:48:39 +0700

    menyesuaikan data dan mengubah sedikit tampilan

--- components/CardDrop.tsx
+++ components/CardDrop.tsx
@@ -9,9 +9,10 @@ import { MovingBorderDemo } from "./MovingBorderDemo";
 // Define a type for the person's data for better type-safety
 interface PersonData {
   name: string;
+  nuptk: string;
   prodi: string;
   job: string;
-  location: string;
+  contact: string;
   imageUrl: string;
   skills: string[];
 }
@@ -40,7 +41,7 @@ function MyCard({ person }: { person: PersonData }) {
       <Card
         className="w-[300px] h-auto overflow-hidden"
         style={{
-          maxHeight: expanded ? 500 : 300,
+          maxHeight: expanded ? 500 : 310,
           transition: "max-height 0.5s ease-in-out",
         }}
       >
@@ -71,9 +72,10 @@ function MyCard({ person }: { person: PersonData }) {
         </CardHeader>
         <CardBody className="flex-col items-center pt-1 overflow-hidden">
           <h3 className="font-bold text-large break-words text-center leading-[1.2] min-h-14 flex items-center justify-center">{person.name}</h3>
-          <p className="pb-2 break-words text-center">{person.prodi}</p>
-          <p className="text-tiny pb-2 break-words text-center">{person.job}</p>
-          <p className="text-tiny text-default-500 pb-2 break-words text-center">{person.location}</p>
+          <p className="pb-1 text-medium break-words text-center">{person.nuptk}</p>
+          <p className="pb-1 break-words text-center">{person.prodi}</p>
+          <p className="pb-2 text-tiny break-words text-center">{person.job}</p>
+          <p className="text-tiny text-default-500 pb-2 break-words text-center">{person.contact}</p>
           <div className={`${styles.detailWrapper} ${showDetail && expanded ? styles.detailVisible : ''}`}>
             <div className="mt-5 w-full px-2">
               <h2 className="mb-2">Expert In:</h2>
@@ -93,80 +95,90 @@ export default function App() {
   const person: PersonData[] = [{
     name: "ABDUL RAHMAD, M.Pd",
     prodi: "Sistem Informasi",
+    nuptk: "123456789",
     job: "Dosen STTI",
-    location: "Tanjungpinang",
+    contact: "0812345678",
     imageUrl: "https://sttindonesia.ac.id/Files/fotodosen/abdul1x1.png",
     skills: ["Mengajar"],
   },
   {
     name: "AURORA ELSA S. FREDERICK, S.E., M.B.A",
+    nuptk: "123456789",
     prodi: "Sistem Informasi",
     job: "Dosen STTI",
-    location: "Tanjungpinang",
+    contact: "0812345678",
     imageUrl: "https://sttindonesia.ac.id/Files/fotodosen/elsa1x1.png",
     skills: ["Mengajar", "Manajemen", "Bisnis", "Pemasaran"],
   },
   {
     name: "DARMAWAN MEGA PERMANA, S.Pi., M.M",
+    nuptk: "123456789",
     prodi: "Sistem Informasi",
     job: "Dosen STTI",
-    location: "Tanjungpinang",
+    contact: "0812345678",
     imageUrl: "https://sttindonesia.ac.id/wp-content/uploads/2025/05/darmawan1x1.png",
     skills: ["Mengajar"],
   },
   {
     name: "DWI NURUL HUDA, S.T., M.Kom",
+    nuptk: "123456789",
     prodi: "Sistem Informasi",
     job: "Dosen STTI",
-    location: "Tanjungpinang",
+    contact: "0812345678",
     imageUrl: "https://sttindonesia.ac.id/Files/fotodosen/dwi1x1.png",
     skills: ["Mengajar"],
   },
   {
     name: "ELVIANNA, M.M",
+    nuptk: "123456789",
     prodi: "Sistem Informasi",
     job: "Dosen STTI",
-    location: "Tanjungpinang",
+    contact: "0812345678",
     imageUrl: "https://sttindonesia.ac.id/Files/fotodosen/elvi1x1.png",
     skills: ["Mengajar"],
   },
   {
     name: "HENDI SETIAWAN, M.Kom",
+    nuptk: "123456789",
     prodi: "Sistem Informasi",
     job: "Dosen STTI",
-    location: "Tanjungpinang",
+    contact: "0812345678",
     imageUrl: "https://sttindonesia.ac.id/Files/fotodosen/hendi1x1.png",
     skills: ["Mengajar"],
   },
   {
     name: "ILIVIA, S.Kom., M.M",
+    nuptk: "123456789",
     prodi: "Sistem Informasi",
     job: "Dosen STTI",
-    location: "Tanjungpinang",
+    contact: "0812345678",
     imageUrl: "https://sttindonesia.ac.id/Files/fotodosen/ilivia1x1.png",
     skills: ["Mengajar"],
   },
   {
     name: "LARASATI INDRIASTUTI, S.E., M.Ak",
+    nuptk: "123456789",
     prodi: "Sistem Informasi",
     job: "Dosen STTI",
-    location: "Tanjungpinang",
+    contact: "0812345678",
     imageUrl: "https://sttindonesia.ac.id/Files/fotodosen/larasati1x1.png",
     skills: ["Mengajar"],
   },
   {
     name: "LEVA AFFRILLIANGGI FALIHAH, S.Kom., M.M",
+    nuptk: "123456789",
     prodi: "Sistem Informasi",
     job: "Dosen STTI",
-    location: "Tanjungpinang",
+    contact: "0812345678",
     imageUrl: "https://sttindonesia.ac.id/wp-content/uploads/2025/05/leva2_1x1.png",
     skills: ["Mengajar"],
   },
   {
     name: "LISKEN SIRAIT, S.Sos., M.Pd",
+    nuptk: "123456789",
     prodi: "Sistem Informasi",
     job: "Dosen STTI",
-    location: "Tanjungpinang",
+    contact: "0812345678",
     imageUrl: "https://sttindonesia.ac.id/Files/fotodosen/lisken1x1.png",
     skills: ["Mengajar"],
   },
