507c00316ad99cea88e8883e01f9a9c191f9f219
YuriFerdinand <muhammadyuri965@gmail.com>
2025-10-07 10:39:42 +0700

    membuat agar img svg menghilang dari dashboard admin

--- app/layout-client.tsx
+++ app/layout-client.tsx
@@ -34,6 +34,8 @@ export function LayoutClient({ children }: { children: React.ReactNode }) {
     return () => clearTimeout(timer);
   }, [pathname]);
 
+  const isPathname = pathname.startsWith('/dashboard');
+
   return (
     <body className={clsx("min-h-screen text-foreground bg-background antialiased", myFont.className)}>
       <div className="relative flex flex-col ">
@@ -42,6 +44,8 @@ export function LayoutClient({ children }: { children: React.ReactNode }) {
         </div>
         <ProdiProvider>
         <Banner />
+
+        {!isPathname && (
         <svg viewBox="0 0 1440 320" xmlns="http://www.w3.org/2000/svg">
           <path
             d="M0,160L60,144C120,128,240,96,360,122.7C480,149,600,235,720,250.7C840,267,960,213,1080,197.3C1200,181,1320,203,1380,213.3L1440,224L1440,0L1380,0C1320,0,1200,0,1080,0C960,0,840,0,720,0C600,0,480,0,360,0C240,0,120,0,60,0L0,0Z"
@@ -49,7 +53,9 @@ export function LayoutClient({ children }: { children: React.ReactNode }) {
             fillOpacity="1"
           />
         </svg>
-        <main ref={mainRef} className="container mx-auto pt-10 px-6 flex-grow">{children}</main>
+        )}
+        
+        <main ref={mainRef} className="container mx-auto flex-grow">{children}</main>
         </ProdiProvider>
       </div>
       <div>
